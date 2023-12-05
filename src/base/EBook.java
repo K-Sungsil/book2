c2b11cc8ae072dc8690fe7b1c555e0d92548fb65
K-Sungsil <148848577+K-Sungsil@users.noreply.github.com>
2023-12-05 19:34:50 +0900

    book2 생성

---
+++ src/base/EBook.java
@@ -0,0 +1,21 @@
+package base;
+
+import java.time.LocalDate;
+
+public class EBook extends Book {
+    private  String fileSize;
+
+    //생성자 생성
+    public EBook(Long id, String name, String author, Long isbn, LocalDate publishedDate, String fileSize) {
+        super(id, name, author, isbn, publishedDate);
+        this.fileSize = fileSize;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+}
---
+++ src/base/Execute.java
@@ -0,0 +1,10 @@
+package base;
+
+public class Execute {
+    static BookManager bm3 = new BM3();
+
+    public static void main(String[] args) {
+        bm3.init();
+        bm3.interactWithUser();
+    }
+}
